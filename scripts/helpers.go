c80806e89d7f8326bb1236acc6bf4d6ed361959c
Oliinant <niilo.abbett@gmail.com>
2025-10-26 02:30:33 +0200

    Add helpers_test.go and update helpers.go
    - Remove unused imports from helpers.go
    - Update WrapError from helpers.go to return nil and add comma
    - Update PtrToElem from helpers.go to be exportable and make return type reflect.Value
    - Update GetInfoStr from helpers.go to be exportable
    - Update GetInfoMap from helpers.go to be exportable
    - Create TestCases struct for helpers.go
    - Create BoilerTestFunc for helpers.go

--- scripts/helpers.go
+++ scripts/helpers.go
@@ -2,28 +2,29 @@ package scripts
 
 import (
 	"fmt"
-	"refelct"
+	"reflect"
+	"testing"
+	"encoding/json"
+	"strings"
 )
 
-func WrapError(context string err error) error {
+func WrapError(context string, err error) error {
 	if err == nil {
-		return 
+		return nil
 	}
 	return fmt.Errorf("%s: %w", context, err)
 }
 
-func ptrToElem(s any) any {
-	if s.Kind() != reflect.Ptr {
-		fmt.Printf("Value %T is already an element\n", s)
-		return s
+func PtrToElem(v reflect.Value) reflect.Value {
+	if v.Kind() != reflect.Ptr {
+		fmt.Printf("Value %T is already an element\n", v)
+		return v
 	}
-	return s.Elem()
+	return v.Elem()
 }
 
-func getInfoStr(s any) (string, error) {
+func GetInfoStr(s any) (string, error) {
 	t := reflect.TypeOf(s)
-	tElem := ptrToElem(t)
-
 	errorMessage := fmt.Sprintf("Failed to return %s data as string", t.Name())
 
 	sJSON, err := json.MarshalIndent(s, "", "  ")
@@ -34,13 +35,12 @@ func getInfoStr(s any) (string, error) {
 	return string(sJSON), nil
 }
 
-func getInfoMap(s any) (map[string]interface{}) {
+func GetInfoMap(s any) (map[string]interface{}) {
 	sData := make(map[string]interface{})
 	v := reflect.ValueOf(s).Elem()
-	t := reflect.Type(s)
 
-	vElem := ptrToElem(v)
-	tElem := ptrToElem(t)
+	vElem := PtrToElem(v)
+	tElem := vElem.Type()
 
 	for i := 0; i < vElem.NumField(); i++ {
 		fieldName := tElem.Field(i).Name
@@ -49,4 +49,39 @@ func getInfoMap(s any) (map[string]interface{}) {
 		sData[strings.ToLower(fieldName)] = fieldValue
 	}
 	return sData
+}
+
+type TestCase[T any, U any] struct { // T = expected type; U = got type
+	Name string
+	Input T
+	Want U
+	WantErr bool
+}
+
+func BoilerTestFunc[T any, U any](
+	t *testing.T,
+	fn func(T) (U, error),
+	testCases []TestCase[T, U],
+) {
+	for _, test := range testCases {
+		t.Run(test.Name, func(t *testing.T) {
+			got, err := fn(test.Input)
+
+			if test.WantErr {
+				if err == nil {
+					t.Errorf("Expected error for input \"%v\", got nil", test.Input)
+				}
+				return
+			}
+
+			if err != nil {
+				t.Errorf("Unexpected error for input \"%v\": %v", test.Input, err)
+				return
+			}
+
+			if !reflect.DeepEqual(got, test.Want) {
+				t.Errorf("For input \"%v\" expected %v, got %v", test.Input, test.Want, got)
+			}
+		})
+	}
 }
---
+++ scripts/helpers_test.go
@@ -0,0 +1,17 @@
+package scripts
+
+func TestWrapError() {
+
+}
+
+func TestPtrToElem() {
+
+}
+
+func TestGetInfoStr() {
+
+}
+
+func TestGetInfoMap() {
+	
+}
